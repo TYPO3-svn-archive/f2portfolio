9324e758037cc1788a72fb9a0afa9d45f8e89f7a
farconada <farconada@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-06-09 21:55:25 +0000

    Nuevo campo en la tabla de Tag. Instalar y desinstalar la extension
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/f2portfolio/trunk@34093 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -40,6 +40,7 @@ CREATE TABLE tx_f2portfolio_domain_model_tag (
 	
 	name tinytext,
 	description tinytext,
+        projects int(11) unsigned DEFAULT '0' NOT NULL,
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
